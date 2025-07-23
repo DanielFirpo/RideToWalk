a5c53f1f87e9e868a483485e288bf8741dc7c733
DanielFirpo <17069338+DanielFirpo@users.noreply.github.com>
2025-07-22 22:10:56 -0700

    Fix Dockerfile to accept Posthog env vars

--- frontend/src/app/_components/PostHogProvider.tsx
+++ frontend/src/app/_components/PostHogProvider.tsx
@@ -1,27 +1,24 @@
-"use client"
+"use client";
 
-import posthog from "posthog-js"
-import { PostHogProvider as PHProvider } from "posthog-js/react"
-import { useEffect } from "react"
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import { useEffect } from "react";
 
 interface PostHogProviderProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export function PostHogProvider({ children }: PostHogProviderProps) {
+  console.log("initializing posthog with keyt", process.env.NEXT_PUBLIC_POSTHOG_KEY);
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: "/ingest",
       ui_host: "https://us.posthog.com",
-      defaults: '2025-05-24',
+      defaults: "2025-05-24",
       capture_exceptions: true,
       debug: process.env.NODE_ENV === "development",
-    })
-  }, [])
+    });
+  }, []);
 
-  return (
-    <PHProvider client={posthog}>
-      {children}
-    </PHProvider>
-  )
+  return <PHProvider client={posthog}>{children}</PHProvider>;
 }
